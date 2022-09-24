7382c7a86c0d17ead2dff0479c5d187e3176be75
S.O.A <48219791+X-Lime22@users.noreply.github.com>
2022-09-24 20:03:02 +0300

    site for photograph

---
+++ site/js/app.js
@@ -0,0 +1,42 @@
+$(function() {
+    let header = $("#header");
+    let introH = $("#works__main").innerHeight();
+    let scrollOffset = $(window).scrollTop();
+
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset) {
+
+        if( scrollOffset >= introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+    /* Menu nav toogle */
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+        $("#header").toggleClass("active");
+    });
+
+    $(document).ready(function(){
+        $('.fade').slick({
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+        });
+    });
+});
